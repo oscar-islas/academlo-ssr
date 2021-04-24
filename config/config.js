35c782eac63a8fabde7477f3644994c99942320d
Oscar Islas <oislasreyes@gmail.com>
2021-04-23 20:17:06 -0500

    modificando la cadena de conexion hacia la DB

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER_,
-    "password": process.env.DB_PASSWORD_,
-    "database": process.env.DB_NAME_,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: process.env.DB_USER_,
+    password: process.env.DB_PASSWORD_,
+    database: process.env.DB_NAME_,
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    use_env_variable: 'DATABASE_URL',
+  production: {
+    use_env_variable: 'DATABASE_URL_AWS',
     dialectOptions: {
       ssl: {
         require: true,
